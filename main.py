9519f27c5a3ec0a6e88a46ea4ff6a926195e497c
Kyle <notxap55@yahoo.com>
2024-07-22 15:26:51 -0700

    completed

--- main.py
+++ main.py
@@ -8,6 +8,27 @@ def move_forward():
     tim.forward(10)
 
 
+def move_backward():
+    tim.backward(10)
+
+
+def turn_clockwise():
+    tim.right(10)
+
+
+def turn_counter_clockwise():
+    tim.left(10)
+
+
+def clear():
+    tim.reset()
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key='s', fun=move_backward)
+screen.onkey(key='a', fun=turn_counter_clockwise)
+screen.onkey(key='d', fun=turn_clockwise)
+screen.onkey(key='c', fun=clear)
+
 screen.exitonclick()
